0be8b80cedf9eeae8cccd7b617dd38ad674eb11e
Tran Hai Trieu <haitrieu2524@gmail.com>
2023-11-05 11:25:51 +0700

    Sửa lỗi upload image same input - output

--- src/services/images.services.ts
+++ src/services/images.services.ts
@@ -1,10 +1,10 @@
 import { Request } from 'express';
+import fsPromise from 'fs/promises';
 import path from 'path';
 import sharp from 'sharp';
 import { UPLOAD_IMAGE_DIR } from '~/constants/dir';
-import fsPromise from 'fs/promises';
 
-import { getNameFromFullname, handleUploadImage } from '~/utils/file';
+import { getExtensionFromFullname, getNameFromFullname, handleUploadImage } from '~/utils/file';
 import { uploadFileToS3 } from '~/utils/s3';
 
 class ImagesService {
@@ -14,15 +14,20 @@ class ImagesService {
     const result = await Promise.all(
       images.map(async (image) => {
         const newName = getNameFromFullname(image.newFilename);
+        const extention = getExtensionFromFullname(image.newFilename);
         const newFullname = `${newName}.jpg`;
         const newPath = path.resolve(UPLOAD_IMAGE_DIR, newFullname);
-        await sharp(image.filepath).jpeg().toFile(newPath);
+        if (extention !== 'jpg') await sharp(image.filepath).jpeg().toFile(newPath);
         await uploadFileToS3({
           filename: `images/${newFullname}`,
           filepath: newPath,
           contentType: 'image/jpeg'
         });
-        await Promise.all([fsPromise.unlink(image.filepath), fsPromise.unlink(newPath)]);
+        try {
+          await Promise.all([fsPromise.unlink(image.filepath), fsPromise.unlink(newPath)]);
+        } catch (error) {
+          console.log(error);
+        }
         return {
           name: newFullname
         };
